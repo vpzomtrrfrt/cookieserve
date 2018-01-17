7217f6caef9acf97ef473c8383ec00ece19a2314
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-01-16 20:39:37 -0700

    make it do fortunes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 extern crate futures;
 extern crate hyper;
+extern crate rand;
 
 use futures::future::Future;
 
-struct CookieService;
+use rand::Rng;
 
-impl hyper::server::Service for CookieService {
+struct CookieService<'a> {
+    fortunes: &'a Vec<String>
+}
+
+impl<'a> hyper::server::Service for CookieService<'a> {
     type Request = hyper::server::Request;
     type Response = hyper::server::Response;
     type Error = hyper::Error;
     type Future = Box<Future<Item=Self::Response,Error=Self::Error>>;
 
     fn call(&self, _req: Self::Request) -> Self::Future {
-        let resp = "test";
+        let resp = rand::thread_rng().choose(self.fortunes).unwrap().to_owned();
         Box::new(futures::future::ok(Self::Response::new()
                                      .with_header(hyper::header::ContentLength(resp.len() as u64))
                                      .with_body(resp)))
@@ -25,8 +30,11 @@ fn main() {
         std::env::var("PORT").unwrap_or("7000".to_owned())
         .parse().unwrap()
         );
+    let fortunes = include_str!("../cookie").split("\n%\n").map(|s|s.to_owned()).collect::<Vec<_>>();
     let server = hyper::server::Http::new()
-        .bind(&addr, || Ok(CookieService))
+        .bind(&addr, move || Ok(CookieService {
+            fortunes: &fortunes
+        }))
         .unwrap();
     server.run().unwrap();
 }
